9aa7d4366f2a21f0b4d5997072d9c7bc8d0b9b0d
Joao Azevedo <joao.c.azevedo@gmail.com>
2022-09-01 17:46:20 +0100

    Add Route53 records

--- _infra/main.tf
+++ _infra/main.tf
@@ -38,3 +38,18 @@ data "aws_iam_policy_document" "jcazevedo_net_allow_public_access" {
     resources = ["${aws_s3_bucket.jcazevedo_net.arn}/*"]
   }
 }
+
+resource "aws_route53_zone" "jcazevedo_net" {
+  name = "jcazevedo.net"
+}
+
+resource "aws_route53_record" "jcazevedo_net-a" {
+  zone_id = aws_route53_zone.jcazevedo_net.zone_id
+  name = "jcazevedo.net"
+  type = "A"
+  alias {
+    name = aws_s3_bucket.jcazevedo_net.website_endpoint
+    zone_id = aws_s3_bucket.jcazevedo_net.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
