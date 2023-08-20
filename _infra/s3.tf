88fc8a267a9a5e85e4cf99fbe0b70cd98e10a62d
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-08-20 13:01:13 +0100

    Update server-side encryption config for S3 buckets

--- _infra/s3.tf
+++ _infra/s3.tf
@@ -2,8 +2,32 @@ resource "aws_s3_bucket" "jcazevedo_net" {
   bucket = "jcazevedo.net"
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "jcazevedo_net" {
+  bucket = aws_s3_bucket.jcazevedo_net.id
+
+  rule {
+    bucket_key_enabled = false
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 resource "aws_s3_bucket" "www_jcazevedo_net" {
   bucket = "www.jcazevedo.net"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "www_jcazevedo_net" {
+  bucket = aws_s3_bucket.www_jcazevedo_net.id
+
+  rule {
+    bucket_key_enabled = false
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
 
 resource "aws_s3_bucket_cors_configuration" "jcazevedo_net" {
